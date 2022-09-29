cb9310fc49950d13685152520b2cac489e2e1863
rebeccawzq <102126673+rebeccawzq@users.noreply.github.com>
2022-09-28 22:14:01 -0400

    Update columbia_student_resource.py

--- src/columbia_student_resource.py
+++ src/columbia_student_resource.py
@@ -32,7 +32,7 @@ class ColumbiaStudentResource:
 
     @staticmethod
     def get_by_key(key):
-        sql = "SELECT * FROM f22_database.columbia_student where uni=%s";
+        sql = "SELECT * FROM f22_database.columbia_student where guid=%s";
         conn = ColumbiaStudentResource._get_connection()
         cur = conn.cursor()
         res = cur.execute(sql, args=key)
